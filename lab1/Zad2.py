b67090e52dff99fd64e5aca87e274ef161c4b11a
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-02-28 09:41:08 +0100

    Create Zad2.py

---
+++ lab1/Zad2.py
@@ -0,0 +1,12 @@
+liczba1 = 6
+liczba2 = 3
+
+dodawanie = liczba1 + liczba2
+odejmowanie = liczba1 - liczba2
+mnozenie = liczba1 * liczba2
+dzielenie =liczba1 / liczba2
+
+print(dodawanie)
+print(odejmowanie)
+print(mnozenie)
+print(dzielenie)
