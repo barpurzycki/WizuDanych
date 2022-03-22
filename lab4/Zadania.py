939eb1bec3d0551fdde005e4a62361b2c7724781
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-03-22 11:27:50 +0100

    Create Zadania.py

---
+++ lab4/Zadania.py
@@ -0,0 +1,35 @@
+#Zad 1
+
+# lista = []
+#
+# for a in range(0, 31):
+#     a = a * 2
+#     lista.append(a)
+#
+# plik = open('zadania.txt', 'w+')
+#
+# plik.writelines(str(lista))
+# plik.close()
+#
+#Zad 2
+#
+# plik = open('zadania.txt', 'r')
+#
+# odczyt = plik.readlines()
+#
+# print(odczyt)
+# plik.close()
+
+#Zad 3
+
+# with open('zadania.txt', 'w') as plik:
+#     for a in range(0, 10):
+#         plik.write('Linijka tekstu \n')
+#
+# with open('zadania.txt', 'r') as plik:
+#     for a in plik:
+#         print(a, end='')
+
+#Zad 4
+
+#class NaZakupy:
