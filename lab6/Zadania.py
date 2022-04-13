d400aaa19d51cc6ccf920002af207061b61c43d2
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-04-13 14:14:02 +0200

    Update Zadania.py

--- lab6/Zadania.py
+++ lab6/Zadania.py
@@ -53,3 +53,57 @@ def macierz(n):
 
     return x
 print(macierz(3))
+
+#Zad 8
+
+def podzial(tablica,kierunek):
+    if(kierunek=="poziomo" or kierunek=="pionowo"):
+        if(tablica.shape[0]%2==1 and kierunek=="poziomo"):
+            return "nie możesz tego zrobić, bo wysokość tablicy jest liczbą nieparzystą"
+        if(tablica.shape[1]%2==1 and kierunek=="pionowo"):
+             return "nie możesz tego zrobić, bo szerokość tablicy jest liczbą nieparzystą"
+        else:
+            if(kierunek=="poziomo" and tablica.shape[0]%2==0):
+                tab1=np.zeros((tablica.shape[0]//2,tablica.shape[1]))
+                tab2 = np.zeros((tablica.shape[0]//2, tablica.shape[1]))
+                for i in range(0,tab1.shape[0]):
+                    for j in range(0,tab1.shape[1]):
+                        tab1[i,j]=tablica[i,j]
+                for i in range(tab2.shape[0],tablica.shape[0]):
+                    for j in range(tab2.shape[0], tablica.shape[0]):
+                        tab2[i-tab2.shape[0], j-tab2.shape[0]] = tablica[i, j]
+                return tab1,tab2
+            if (kierunek == "pionowo" and tablica.shape[1] % 2 == 0):
+                tab1 = np.zeros((tablica.shape[0], tablica.shape[1]//2))
+                tab2 = np.zeros((tablica.shape[0], tablica.shape[1]//2))
+                for i in range(0, tab1.shape[0]):
+                    for j in range(0, tab1.shape[1]):
+                        tab1[i, j] = tablica[i, j]
+                for i in range(tab2.shape[0], tablica.shape[0]):
+                    for j in range(tab2.shape[0], tablica.shape[0]):
+                        tab2[i - tab2.shape[0], j - tab2.shape[0]] = tablica[i, j]
+                return tab1, tab2
+
+
+    else:
+        return "nastepnym razem wpisz jako kierunek 'pionowo' lub 'poziomo'"
+
+
+#Zad 9
+
+r=input("podaj róznicę(int)")
+a1=input("podaj 1 wyraz ciągu(int)")
+r=int(r)
+a1=int(a1)
+
+list=[a1+r*i for i in range(0,25)]
+
+
+macierz=np.zeros((5,5))
+licznik=0
+for i in range(0,5):
+    for j in range(0,5):
+        macierz[i,j]=list[licznik]
+        licznik+=1
+
+print(macierz)
