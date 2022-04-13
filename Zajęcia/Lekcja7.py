5fec608f827906017d97d40cc2db72b5960ef486
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-04-13 14:11:34 +0200

    Create Lekcja7.py

---
+++ Zajęcia/Lekcja7.py
@@ -0,0 +1,56 @@
+import numpy as np
+# a = np.array([20, 30, 40, 50])
+# b = np.array(4)
+# b2 = ([2,4,5,6])
+# c = a + b
+# print(c)
+# print(b**2)
+#
+# print(np.exp(b))
+# print(np.sort(b2))
+# d = np.sort(b2)
+# e = d + b
+# print(e)
+
+
+
+# a = np.arange(12).reshape((3,4))
+# print(a)
+# print(a.sum())
+# print(a.sum(axis=0))
+# print(a.sum(axis=1))
+# print(a.cumsum(axis=1))
+
+
+
+# a = np.arange(3)
+# b = np.arange(3)
+#
+# print(a.dot(b))
+#
+# c = np.array([[2, 3, 6], [5, 1, 3]])
+# d = np.array([[1, 4], [2, 1], [3, 5]])
+# e = c.dot(d)
+# print(e)
+
+
+# a = np.arange(6).reshape(3, 2)
+# print(a)
+# for b in a:
+#     for c in b:
+#         print(b)
+
+# for b in a.flat:
+#     print(b)
+
+
+# a = np.arange(6)
+# print(a)
+# b = a.reshape([3,2])
+# c = a.reshape([2,3])
+# print(b)
+# print(c)
+# d = c.ravel()
+# print(d)
+# e = c.T
+# print(e)
