9e24ff9e9af5b53aa353082430cf8cb32671ce54
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-05-13 09:47:01 +0200

    Update Zadania.py

--- lab10/Zadania.py
+++ lab10/Zadania.py
@@ -1,9 +1,20 @@
 #Zad 1
 
 x=np.arange(1, 20, 1)
+plt.plot(x, 1/x')
+plt.axis([0,20,0,1])
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.legend(labels=['f(x)= 1/x'])
+plt.show()
+
+#Zad 2
+
+x=np.arange(1, 20, 1)
 plt.plot(x, 1/x, 'g:^')
 plt.axis([0,20,0,1])
 plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.legend(labels=['f(x)= 1/x'])
+plt.title("wykres funkcji f(x) dla x [1,20]")
 plt.show()
