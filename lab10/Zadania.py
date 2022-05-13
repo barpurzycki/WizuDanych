90ff70974ba36141d0d76504d083c717e3f7e3e8
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-05-13 09:53:20 +0200

    Update Zadania.py

--- lab10/Zadania.py
+++ lab10/Zadania.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import pandas as pd
+
 #Zad 1
 
 x=np.arange(1, 20, 1)
@@ -17,4 +22,16 @@ plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.legend(labels=['f(x)= 1/x'])
 plt.title("wykres funkcji f(x) dla x [1,20]")
+plt.show()
+         
+#Zad 3
+
+x = np.arange(0, 30, 0.1)
+sin = np.sin(x)
+cos = np.cos(x)
+plt.plot(x, sin, label="sin(x)")
+plt.plot(x, cos, label="cos(x)")
+plt.legend(loc='upper left')
+plt.xlabel("x")
+plt.ylabel("sin(x) i cos(x)")
 plt.show()
